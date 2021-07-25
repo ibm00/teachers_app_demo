26e23658acbe3305445c8ea64de289e853edd3cf
F-BONAPARTA <67749770+F-BONAPARTA@users.noreply.github.com>
2021-07-25 22:50:54 +0200

    oi

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -14,13 +14,6 @@ import 'news_part.dart';
 import 'profile/profile_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  String getUserFirstName(String n) {
-    String name = '';
-    List<String> l = n.split(' ');
-    name = l.first;
-    return name.length > 10 ? name.substring(0, 9) : name;
-  }
-
   @override
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
@@ -35,12 +28,11 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: h * 0.02,
             ),
-
             Consumer(
               builder: (context, watch, child) {
                 final provider = watch(userDataProvider);
                 return Text(
-                  "مرحبا " + getUserFirstName(provider.fullName),
+                  provider.fullName,
                   style:
                       TextStyle(fontSize: w * 0.1, fontWeight: FontWeight.w600),
                 );
@@ -49,57 +41,48 @@ class HomeScreen extends StatelessWidget {
             Text(
               'كيف يمكننا مساعدتك اليوم ؟',
               style: TextStyle(
-                fontSize: w * 0.06,
+                fontSize: w * 0.07,
                 color: Colors.grey[600],
               ),
             ),
             SizedBox(
               height: h * 0.02,
             ),
-            // _buildNewsPart(h, w, or),
             NewsPartScreen(),
-            RepaintBoundary(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (ctx) => QuizHomeScreen()));
-                },
-                child: SvgPicture.asset(
-                  'assets/images/quizes_home_screen_card.svg',
-                  width: w * 0.95,
-                ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => QuizHomeScreen(),
+                  ),
+                );
+              },
+              child: HomeListMember(
+                w: w,
+                itemColor: const Color(0xFF4C66D5),
+                title: "الإمتحانات",
+                des: "آخر التدريبات والامتحانات\nالمضافة",
+                path: "assets/images/test.png",
               ),
             ),
-
-            RepaintBoundary(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (ctx) => LessionsHomeScreen()));
-                },
-                child: SvgPicture.asset(
-                  'assets/images/lessons_home_screen_card.svg',
-                  width: w * 0.95,
-                ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => LessionsHomeScreen(),
+                  ),
+                );
+              },
+              child: HomeListMember(
+                w: w,
+                itemColor: const Color(0xFF383D41),
+                title: "الدروس",
+                des: "شاهد آخر الدروس المضافه",
+                path: "assets/images/read.png",
               ),
             ),
-
-            //  Stack(
-            //   fit: StackFit.expand,
-            //   children: [
-            //     Container(
-            //       height: 200,
-            //       decoration: BoxDecoration(
-            //         color: Color(0xFF383D41),
-            //         borderRadius: BorderRadius.circular(20),
-            //       ),
-
-            //     ),
-
-            //   ],
-            // ),
           ],
         ),
       )),
@@ -121,9 +104,11 @@ class HomeScreen extends StatelessWidget {
                 showCustomToast(message);
               }
             },
-            child: SvgPicture.asset(
-              'assets/images/barcode_icon.svg',
-              width: w * 0.08,
+            child: RepaintBoundary(
+              child: SvgPicture.asset(
+                'assets/images/barcode_icon.svg',
+                width: w * 0.08,
+              ),
             ),
           ),
           const Spacer(),
@@ -136,19 +121,109 @@ class HomeScreen extends StatelessWidget {
                       curve: Curves.bounceIn,
                       child: ProfileScreen()));
             },
-            child: SvgPicture.asset(
-              'assets/images/menu_icon.svg',
-              width: w * 0.1,
+            child: RepaintBoundary(
+              child: SvgPicture.asset(
+                'assets/images/menu_icon.svg',
+                width: w * 0.1,
+              ),
             ),
           ),
           const Spacer(),
           GestureDetector(
-            onTap: () {},
-            child: SvgPicture.asset(
-              'assets/images/notification_icon.svg',
-              width: w * 0.1,
+            onTap: () {
+              Map map = VideoHelper.getValidUrlForVideo(
+                  'https://www.youtube.com/watch?v=QLcGs-eosOE&list=RDQLcGs-eosOE&start_radio=1&ab_channel=Nota-%D9%86%D9%88%D8%AA%D9%87');
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => VideoPreviewScreen(
+                            url: map['url'] as String,
+                            isYouTube: map['isYouTube'] as bool,
+                          )));
+            },
+            child: RepaintBoundary(
+              child: SvgPicture.asset(
+                'assets/images/notification_icon.svg',
+                width: w * 0.1,
+              ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeListMember extends StatelessWidget {
+  const HomeListMember({
+    required this.w,
+    required this.itemColor,
+    required this.title,
+    required this.des,
+    required this.path,
+  });
+
+  final double w;
+  final Color itemColor;
+  final String title;
+  final String des;
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: itemColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            width: w * 0.95,
+            height: w * 0.377,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 7),
+                child: Image.asset(
+                  path,
+                  width: w * 0.3,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      des,
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0x80FFFFFF),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
